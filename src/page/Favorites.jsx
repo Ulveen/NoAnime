41d704d0811e84621b8edb41ae852a3a483df823
Alvin Setiono <setionoalvin9@gmail.com>
2023-05-02 11:55:03 +0700

    hai

--- src/page/Favorites.jsx
+++ src/page/Favorites.jsx
@@ -5,13 +5,17 @@ import { Theme } from "../context/Theme"
 export default function Favorites(){
     const list = JSON.parse(localStorage.getItem("anime"))
     let theme = useContext(Theme)
-    if(list.length===0){
-        return <h1>List is Empty</h1>
+    if(list===null || list.length===0){
+        return (
+            <div>
+                <div className="fill" style={theme}></div>
+                <h1>List is Empty</h1>
+            </div>
+        )
     }
     else{
         return(
             <div>
-                <div className="fill" style={theme}></div>
                 <h1 className='title' style={theme.title}>Favorites</h1>
                 {list.map((anime, index) => {
                     return <ListComponent anime = {anime}/>
